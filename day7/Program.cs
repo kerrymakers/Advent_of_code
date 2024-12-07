4f35b3a42d11309b52ef2f9d979076d1b3e93ea4
Kerry Finch <kerry@makers.tech>
2024-12-07 14:56:16 +0000

    add day 7, I should really start committing more often lol

---
+++ day7/Program.cs
@@ -0,0 +1,75 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string inputFile = "/Users/kerryfinch/Documents/Projects/Advent_of_code/day7/input.txt";
+        string[] input = File.ReadAllLines(inputFile);
+        string[] splitInput;
+        decimal answer;
+        string[] numbers;
+        //find number of gaps
+        //loop through each operation sequence 
+        //e.g. if 3 gaps - +++, ++*, +*+, *++, +**, *+*, **+, ***
+        List<string> allOperations;
+        //string operations = ""; 
+        decimal runningTotal = 0;
+        decimal finalAnswer = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            splitInput = input[i].Split(": ");
+            answer = decimal.Parse(splitInput[0]);
+            numbers = splitInput[1].Split(' ');
+            allOperations = FindOperations(numbers.Length-1);
+
+            foreach(string operation in allOperations)
+            {
+                runningTotal = decimal.Parse(numbers[0]);
+                for (int j = 0; j < operation.Length; j++)
+                {
+                    if (operation[j] == '+')
+                    {
+                        runningTotal += decimal.Parse(numbers[j+1]);
+                    }
+                    else if (operation[j] == '*')
+                    {
+                        runningTotal *= decimal.Parse(numbers[j+1]);
+                    }
+                    else if (operation[j] == '|')
+                    {
+                        runningTotal = decimal.Parse(runningTotal.ToString() + numbers[j+1]);
+                    }
+                }
+                if (runningTotal == answer)
+                {
+                    finalAnswer += answer;
+                    break;
+                }
+            }
+        }
+        Console.WriteLine("Part 2: " + finalAnswer.ToString());
+    }
+
+    static List<string> FindOperations(int numbersLength)
+    {
+        List<string> opList = [];
+        Dive("+*|", numbersLength, opList);
+        return opList;
+    }
+
+    static void Dive(string validChars, int maxLength, List<string> opList, string prefix = "", int level = 0)
+    {
+        level += 1;
+        foreach (char c in validChars)
+        {
+            if (level == maxLength)
+            {
+                opList.Add(prefix + c);
+            }
+            if (level < maxLength)
+            {
+                Dive(validChars, maxLength, opList, prefix + c, level);
+            }
+        }
+    }
+}
